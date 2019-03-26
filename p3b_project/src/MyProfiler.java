d29321ebc95f5d6e433cc7aeb330e0bf77acbf16
William Ju <41123023+Nvulkan@users.noreply.github.com>
2019-03-25 20:28:23 -0500

    Add files via upload

--- p3b_project/src/MyProfiler.java
+++ p3b_project/src/MyProfiler.java
@@ -23,33 +23,36 @@ public class MyProfiler<K extends Comparable<K>, V> {
     TreeMap<K, V> treemap;
     
     public MyProfiler() {
-        // TODO: complete the Profile constructor
         // Instantiate your HashTable and Java's TreeMap
+    	hashtable = new HashTable();
+    	treemap = new TreeMap();
     }
     
-    public void insert(K key, V value) {
-        // TODO: complete insert method
+    public void insert(K key, V value) throws IllegalNullKeyException, DuplicateKeyException {
         // Insert K, V into both data structures
+    	hashtable.insert(key, value);
+    	treemap.put(key, value);
     }
     
-    public void retrieve(K key) {
-        // TODO: complete the retrieve method
+    public void retrieve(K key) throws IllegalNullKeyException, KeyNotFoundException {
         // get value V for key K from data structures
+    	hashtable.get(key);
+    	treemap.get(key);
     }
     
     public static void main(String[] args) {
         try {
             int numElements = Integer.parseInt(args[0]);
-
-            
-            // TODO: complete the main method. 
             // Create a profile object. 
-            // For example, Profile<Integer, Integer> profile = new Profile<Integer, Integer>();
+            MyProfiler<Integer, Integer> profile = new MyProfiler<Integer, Integer>();
             // execute the insert method of profile as many times as numElements
+            for (int i=0; i<numElements; i++) {
+            	profile.insert(i, i);
+            }
             // execute the retrieve method of profile as many times as numElements
-            // See, ProfileSample.java for example.
-            
-        
+            for (int i=0; i<numElements; i++) {
+            	profile.retrieve(i);
+            }
             String msg = String.format("Inserted and retreived %d (key,value) pairs", numElements);
             System.out.println(msg);
         }
